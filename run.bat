34a6f3d81747f0f527d2d03d5100a457dc2eaf34
s3rv3race <roberts.vms@gmail.com>
2026-04-26 19:57:57 -0400

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Momentum Scanner in background...
-start "" /B pythonw "%~dp0flask_app.py" > "%~dp0scanner.log" 2>&1
-timeout /t 2 /nobreak >nul
-echo Server running at http://localhost:5000
-echo Log: %~dp0scanner.log
-start "" http://localhost:5000
-echo.
-echo To stop the scanner, run stop.bat
